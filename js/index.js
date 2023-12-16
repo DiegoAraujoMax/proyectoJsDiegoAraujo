b345e7b007c2a2d91563a5e04e7077fe55a978d6
DiegoAraujoMax <araujo.diego.max@gmail.com>
2023-12-16 17:38:48 -0300

    Cambios para preEntrega2 agregado de array, objetos, entradas y salidas

--- js/index.js
+++ js/index.js
@@ -1,45 +1,10 @@
+let nombreUsuario;
+let edadUsuario;
+let carrito = [];
 
-/*const realizarPreguntas = () => {
-    let preguntaNombre = prompt("Por favor, ¿podría decirnos su nombre y apellido?").toLowerCase();
-    alert("Hola " + preguntaNombre + ", encantados de tenerte aquí");
-
-    let edadValida = false;
-
-    do {
-        let preguntaEdad = parseInt(prompt("Cuentanos, ¿qué edad tienes?"));
-
-        if (isNaN(preguntaEdad) || preguntaEdad < 0) {
-            alert("Por favor, ingrese una edad válida.");
-        } else {
-            edadValida = true;
-
-            if (preguntaEdad < 18) {
-                alert("Usted es menor de edad y no puede ingresar al sitio.");
-                return; // Sale de la función si es menor de 18
-            } else {
-                alert("Usted es mayor de edad y puede seguir.");
-            }
-        }
-    } while (!edadValida);
-
-    let tipoBebida = prompt("¿Qué tipo de bebida desea?");
-    alert("Gracias por su elección. Disfrute de su catálogo de " + tipoBebida + ".");
-};
-
-alert("Bienvenido a nuestra bodega online. Le realizaremos unas breves preguntas a continuación...");
-realizarPreguntas();*/
-
- 
-
-// Variables necesarias
-var nombreUsuario;
-var edadUsuario;
-var carrito = [];
-
-// Función para preguntar nombre y edad
 function ingresarSitio() {
-  nombreUsuario = prompt("Bienvenido al mini e-commerce. Por favor, ingresa tu nombre:");
-  edadUsuario = parseInt(prompt("Hola " + nombreUsuario + ". Ingresa tu edad:"));
+  nombreUsuario = prompt("Bienvenido al a la bodega online. Por favor, ingresa tu nombre:");
+  edadUsuario = parseInt(prompt("Hola " + nombreUsuario + ", ingresa tu edad:"));
 
   if (isNaN(edadUsuario) || edadUsuario < 18) {
     alert("Lo siento, debes ser mayor de 18 años para ingresar al sitio.");
@@ -49,10 +14,9 @@ function ingresarSitio() {
   mostrarOpcionesCompra();
 }
 
-// Función para mostrar opciones de compra
 function mostrarOpcionesCompra() {
-  var opciones = "Elige una opción:\n1. Vinos\n2. Cerveza\n3. Whisky\n4. Otros\n5. Ir al carrito";
-  var eleccion = parseInt(prompt(opciones));
+  let opciones = "Elige la categoria y luego el producto:\n1. Vinos\n2. Cerveza\n3. Whisky\n4. Otros\n5. Ir al carrito";
+  let eleccion = parseInt(prompt(opciones));
 
   switch (eleccion) {
     case 1:
@@ -76,15 +40,42 @@ function mostrarOpcionesCompra() {
   }
 }
 
-// Función para agregar productos al carrito
 function agregarAlCarrito(categoria) {
-  var tiposBebida = ["Tipo A", "Tipo B", "Tipo C"];
-  var precios = [10, 15, 20];
+  let tiposBebida = [];
+  let precios = [];
 
-  var eleccionTipo = parseInt(prompt("Elige un tipo de " + categoria + ":\n1. " + tiposBebida[0] + " ($" + precios[0] + ")\n2. " + tiposBebida[1] + " ($" + precios[1] + ")\n3. " + tiposBebida[2] + " ($" + precios[2] + ")"));
+  switch (categoria) {
+    case "Vinos":
+      tiposBebida = ["Tinto", "Blanco", "Merlot"];
+      precios = [299, 499, 799];
+      break;
+    case "Cerveza":
+      tiposBebida = ["Rubia", "Negra", "Roja"];
+      precios = [140, 200, 160];
+      break;
+    case "Whisky":
+      tiposBebida = ["Escocés", "Irlandés", "Inglés"];
+      precios = [5500, 3600, 900];
+      break;
+    case "Otros":
+      tiposBebida = ["Vodka", "Gin", "Fernet"];
+      precios = [449, 789, 536];
+      break;
+    default:
+      alert("Categoría no válida.");
+      mostrarOpcionesCompra();
+      return;
+  }
 
-  if (eleccionTipo >= 1 && eleccionTipo <= 3) {
-    var producto = {
+  let opciones = "Elige un tipo de " + categoria + ":\n";
+  for (let i = 0; i < tiposBebida.length; i++) {
+    opciones += (i + 1) + ". " + tiposBebida[i] + " ($" + precios[i] + ")\n";
+  }
+
+  let eleccionTipo = parseInt(prompt(opciones));
+
+  if (eleccionTipo >= 1 && eleccionTipo <= tiposBebida.length) {
+    let producto = {
       categoria: categoria,
       tipo: tiposBebida[eleccionTipo - 1],
       precio: precios[eleccionTipo - 1]
@@ -92,19 +83,18 @@ function agregarAlCarrito(categoria) {
 
     carrito.push(producto);
 
-    var continuar = confirm("Producto agregado al carrito. ¿Quieres agregar otro producto?");
+    let continuar = confirm("Producto agregado al carrito. ¿Quieres agregar otro producto?");
     if (continuar) {
       mostrarOpcionesCompra();
     } else {
       mostrarResumenCompra();
     }
   } else {
-    alert("Opción no válida. Por favor, elige una opción del 1 al 3.");
+    alert("Opción no válida. Por favor, elige una opción del 1 al " + tiposBebida.length + ".");
     agregarAlCarrito(categoria);
   }
 }
 
-// Función para mostrar el carrito y permitir eliminar productos
 function mostrarCarrito() {
   if (carrito.length === 0) {
     alert("El carrito está vacío. Agrega productos antes de ir al carrito.");
@@ -112,7 +102,7 @@ function mostrarCarrito() {
     return;
   }
 
-  var montoTotal = 0;
+  let montoTotal = 0;
 
   console.log("Carrito de compras:");
   carrito.forEach(function (producto) {
@@ -120,15 +110,15 @@ function mostrarCarrito() {
     montoTotal += producto.precio;
   });
 
-  var eliminarProducto = confirm("Monto total a pagar: $" + montoTotal + "\n¿Quieres eliminar algún producto del carrito?");
+  let eliminarProducto = confirm("Monto total a pagar: $" + montoTotal + "\n¿Quieres eliminar algún producto del carrito?");
   if (eliminarProducto) {
     mostrarCarritoEliminar();
   } else {
-    var confirmarPago = confirm("¿Quieres proceder al pago?");
+    let confirmarPago = confirm("¿Quieres proceder al pago?");
     if (confirmarPago) {
       alert("Gracias por tu compra, " + nombreUsuario + ". ¡Vuelve pronto!");
     } else {
-      var volverAlMenu = confirm("¿Quieres volver al menú de elección de productos?");
+      let volverAlMenu = confirm("¿Quieres volver al menú de elección de productos?");
       if (volverAlMenu) {
         mostrarOpcionesCompra();
       } else {
@@ -138,14 +128,13 @@ function mostrarCarrito() {
   }
 }
 
-// Función para mostrar el carrito y permitir eliminar productos
 function mostrarCarritoEliminar() {
   console.log("Carrito actual:");
   carrito.forEach(function (producto, index) {
     console.log(index + 1 + ". " + producto.categoria + " - " + producto.tipo + ": $" + producto.precio);
   });
 
-  var indiceEliminar = parseInt(prompt("Ingresa el número del producto que deseas eliminar (o 0 para finalizar):"));
+  let indiceEliminar = parseInt(prompt("Ingresa el número del producto que deseas eliminar (o 0 para finalizar):"));
 
   if (indiceEliminar >= 1 && indiceEliminar <= carrito.length) {
     carrito.splice(indiceEliminar - 1, 1);
@@ -158,9 +147,8 @@ function mostrarCarritoEliminar() {
   }
 }
 
-// Función para mostrar el resumen de la compra y el monto total a pagar
 function mostrarResumenCompra() {
-  var montoTotal = 0;
+  let montoTotal = 0;
 
   console.log("Resumen de la compra:");
   carrito.forEach(function (producto) {
@@ -168,17 +156,17 @@ function mostrarResumenCompra() {
     montoTotal += producto.precio;
   });
 
-  var confirmarPago = confirm("Monto total a pagar: $" + montoTotal + "\n¿Quieres proceder al pago?");
+  let confirmarPago = confirm("Monto total a pagar: $" + montoTotal + "\n¿Quieres proceder al pago?");
 
   if (confirmarPago) {
-    var eliminarProducto = confirm("¿Quieres eliminar algún producto del carrito?");
+    let eliminarProducto = confirm("¿Quieres eliminar algún producto del carrito?");
     if (eliminarProducto) {
       mostrarCarritoEliminar();
     } else {
       alert("Gracias por tu compra, " + nombreUsuario + ". ¡Vuelve pronto!");
     }
   } else {
-    var volverAlMenu = confirm("¿Quieres volver al menú de elección de productos?");
+    let volverAlMenu = confirm("¿Quieres volver al menú de elección de productos?");
     if (volverAlMenu) {
       mostrarOpcionesCompra();
     } else {
@@ -187,5 +175,5 @@ function mostrarResumenCompra() {
   }
 }
 
-// Iniciar el proceso al cargar la página
 ingresarSitio();
+
